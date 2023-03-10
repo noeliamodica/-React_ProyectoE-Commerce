9b85867174ec65d63128ff034786f9a05f49b3af
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-10 19:49:58 -0300

    completo CheckOutPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import NavBar from './components.js/NavBar';
 import Products from './components.js/Products';
+import CheckOutPage from './components.js/CheckOutPage';
 
 function App() {
   return (
     <div className="App">
       
        <NavBar />
+        <CheckOutPage />
        <Products />
       {/*<Product /> */} 
     </div>
---
+++ src/components.js/CheckOutPage.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import products from '../product-data';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+
+
+import Product from './Product';
+
+const CheckOutPage  = () =>{
+    //const [{basket }, dispatch ]= useStateValue();
+
+function FormRow(){
+    return (
+        <React.Fragment>
+            {products.map ((item)=> (
+                <Grid item xs={12} sm={8} md={6} lg={4}>
+                <Product key={item.id} product={item} />
+           </Grid>
+
+            )
+            )}
+        </React.Fragment>
+
+    )
+};
+    return (
+    <div className='{classes.root}'>
+            <Grid container spacing ={3} >
+                <Grid item xs={12} >
+                    <Typography align='center' gutterBottom variant='h4' >
+                        Shopping Card
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={8} md={9} container spacing={2} >
+                    <FormRow  />
+                </Grid>
+                <Grid item xs={12} sm={4} md={3} >
+                <Typography align='center' gutterBottom variant='h4' >
+                        Total
+                    </Typography>
+                      
+                </Grid>
+
+           </Grid>
+
+    </div>
+
+)
+}
+
+export default CheckOutPage;
