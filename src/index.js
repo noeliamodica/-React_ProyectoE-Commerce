dd5f355561b98ed6caccd1eb124bf8a7f54775fd
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-11 12:31:23 -0300

    trabajo con reducer y StateProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {StateProvider} from './components.js/StateProvider';
+import { initialState } from './reducer';
+import reducer from './reducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer} >
+    <App />  
+    </StateProvider>
+    
   </React.StrictMode>
 );
 
